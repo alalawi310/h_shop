91b60eda6d001433c690b69fd5ee78a99e7a436c
Ali AlAlawi <alalawi310@gmail.com>
2021-03-23 01:36:21 +0300

    Initial commit

---
+++ lib/screens/home/home_screen.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:shop_app/constants.dart';
+import 'package:shop_app/screens/home/components/body.dart';
+
+class HomeScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: buildAppBar(),
+      body: Body(),
+    );
+  }
+
+  AppBar buildAppBar() {
+    return AppBar(
+      backgroundColor: Colors.white,
+      elevation: 0,
+      leading: IconButton(
+        icon: SvgPicture.asset("assets/ icons/back.svg"),
+        onPressed: () {},
+      ),
+      actions: [
+        IconButton(
+          icon: SvgPicture.asset(
+            "assets/icons/cart.svg",
+            color: kTextColor,
+          ),
+          onPressed: () {},
+        ),
+        SizedBox(width: kDefaultPadding / 2)
+      ],
+    );
+  }
+}
